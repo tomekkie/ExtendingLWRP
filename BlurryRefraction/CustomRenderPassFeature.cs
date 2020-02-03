e55ee4209b409329016d8a1a0522f8a2e33fad64
Tomasz Kielski <tomekkie@poczta.onet.pl>
2020-02-03 15:01:37 +0100

    Add files via upload

---
+++ BlurryRefraction/CustomRenderPassFeature.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+
+public class CustomRenderPassFeature : UnityEngine.Rendering.Universal.ScriptableRendererFeature
+{
+    class CustomRenderPass : UnityEngine.Rendering.Universal.ScriptableRenderPass
+    {
+        // This method is called before executing the render pass. 
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in an performance manner.
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref UnityEngine.Rendering.Universal.RenderingData renderingData)
+        {
+        }
+
+        /// Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+        }
+    }
+
+    CustomRenderPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        m_ScriptablePass = new CustomRenderPass();
+
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = UnityEngine.Rendering.Universal.RenderPassEvent.AfterRenderingOpaques;
+    }
+    
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(UnityEngine.Rendering.Universal.ScriptableRenderer renderer, ref UnityEngine.Rendering.Universal.RenderingData renderingData)
+    {
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
